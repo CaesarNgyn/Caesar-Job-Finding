a0e7d6acc89a8ed7f48cd5fea1bdfb750902da18
Caesar Ngyn <caesarngyn@gmail.com>
2023-06-28 23:18:43 +0700

    configService fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config/dist';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [AppService],
 })
 export class AppModule { }
