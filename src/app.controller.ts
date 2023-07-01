bac1d79553efad67e1a68ff4537897c15bb9aa41
Caesar Ngyn <caesarngyn@gmail.com>
2023-07-01 19:02:42 +0700

    sign JWT for user when authenticated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,17 @@ import { AppService } from './app.service';
 import { Request, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { LocalAuthGuard } from './auth/guard/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user)
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,24 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport/dist';
 import { LocalStrategy } from './passport/local.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [PassportModule, UsersModule]
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: '60s',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [AuthService]
 })
 export class AuthModule { }
