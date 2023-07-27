927d94da209e8d2fbe0adb53ff8a40d75a39f758
Caesar Ngyn <caesarngyn@gmail.com>
2023-07-27 17:06:40 +0700

    dynamic rendering mail content

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -3,24 +3,53 @@ import { MailService } from './mail.service';
 import { Public } from 'src/decorators/public.decorator';
 import { ResponseMessage } from 'src/decorators/message.decorator';
 import { MailerService } from '@nestjs-modules/mailer';
+import { InjectModel } from '@nestjs/mongoose';
+import { SubscriberDocument } from 'src/subscribers/schemas/subscriber.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
+import { Subscriber } from 'rxjs';
 
 @Controller('mail')
 export class MailController {
   constructor(
     private readonly mailService: MailService,
-    private readonly mailerService: MailerService
+    private readonly mailerService: MailerService,
+
+    @InjectModel(Subscriber.name) private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+    @InjectModel(Job.name) private jobModel: SoftDeleteModel<JobDocument>
+
   ) { }
 
   @Get()
   @Public()
   @ResponseMessage("Test email")
   async handleTestEmail() {
-    await this.mailerService.sendMail({
-      to: "caesarngyn@gmail.com",
-      from: '"From Caesar with love"', // override default from
-      subject: 'Tuyển dụng việc làm',
-      template: 'job.hbs', // HTML body content
-    });
+    const subscribers = await this.subscriberModel.find({});
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills;
+      const jobWithMatchingSkills = await this.jobModel.find({ skills: { $in: subsSkills } });
+      if (jobWithMatchingSkills?.length > 0) {
+        const jobs = jobWithMatchingSkills.map((job, index) => {
+          return {
+            name: job.name,
+            company: job.company.name,
+            salary: `${job.salary}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + " đ",
+            skills: job.skills
+          }
+        })
+        await this.mailerService.sendMail({
+          to: "caesarngyn@gmail.com",
+          from: '"From Caesar with love"', // override default from
+          subject: 'Tuyển dụng việc làm',
+          template: 'job.hbs', // HTML body content
+          context: {
+            jobs,
+            subName: subs.name
+          }
+        });
+      }
+    }
   }
+
 
 }
